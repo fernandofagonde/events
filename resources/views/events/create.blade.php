d4ceda3637bd1b63ec6e6418c08d75214e234a24
Fernando Fagonde <fernando@ipsillon.cc>
2021-10-25 10:12:53 -0300

    ajuste do form de criação de eventos

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,7 +3,32 @@
 @section('title', 'Criar Evento')
 
 @section('content')
-<h1> Criar um evento</h1>
-    
+
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1> Crie o seu evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento" >
+        </div>
+        <div class="form-group">
+            <label for="city">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do Evento" >
+        </div>
+        <div class="form-group">
+            <label for="private">Evento privado?</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="description">Descrição:</label>
+            <textarea type="textarea" class="form-control" id="description" name="description" placeholder="Descrição do Evento" ></textarea>
+        </div>
+        <input type="submit" value="Criar Evento" class="btn btn-primary">
+    </form>
+</div>    
 
 @endsection
